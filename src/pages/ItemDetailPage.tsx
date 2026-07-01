bebcff081b9ea7bc2b533024f82ebab47f95fe63
Dennis <denworksw@gmail.com>
2026-07-01 19:24:39 -0400

    import all catalog pages to know that they exist. tsx file setup

---
+++ src/pages/ItemDetailPage.tsx
@@ -0,0 +1,5 @@
+function ItemDetailPage() {
+  return <h1>Item Detail Page</h1>;
+}
+
+export default ItemDetailPage;
---
+++ src/pages/StatusListPage.tsx
@@ -0,0 +1,5 @@
+function StatusListPage() {
+  return <h1>Status List Page</h1>;
+}
+
+export default StatusListPage;
